972fdb57862976fafd13d6c1a1b84b2319628c81
David <itsdavid71@gmail.com>
2019-09-02 22:17:23 +0300

    Update

---
+++ template-shop/printers-main.php
@@ -0,0 +1,49 @@
+<div class="printers-main">
+    <div class="container">
+        <div class="row aic printer-block">
+            <div class="col-md-6">
+                <div class="printer-name">DESIGNER X</div>
+                <p>Designer X - высокопроизводительный 3D принтер профессионального класса, инструмент для реализации любых идей.</p>
+                <p>Новый принтер объединил в себе надежность легендарного PICASO 3D Designer, а также качество и стабильность новой платформы Х. Designer X всегда готов к работе. Высочайшее разрешение (слой от 0,01 мм) и производительность печати (до 100 см3/час) делают этот принтер уникальным.</p>
+                <div class="flex aic">
+                    <div class="main-button">ЗАКАЗАТЬ</div>
+                    <div class="more-info-button">ПОДРОБНЕЕ</div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="printer-image printer-image-1"></div>
+                <div class="printer-price price">269 000</div>
+            </div>
+        </div>
+        <div class="row aic printer-block">
+             <div class="col-md-6">
+                <div class="printer-image printer-image-2"></div>
+                <div class="printer-price price">269 000</div>
+            </div>
+            <div class="col-md-6">
+                <div class="printer-name">DESIGNER XL</div>
+                <p>Designer X - высокопроизводительный 3D принтер профессионального класса, инструмент для реализации любых идей.</p>
+                <p>Новый принтер объединил в себе надежность легендарного PICASO 3D Designer, а также качество и стабильность новой платформы Х. Designer X всегда готов к работе. Высочайшее разрешение (слой от 0,01 мм) и производительность печати (до 100 см3/час) делают этот принтер уникальным.</p>
+                <div class="flex aic">
+                    <div class="main-button">ЗАКАЗАТЬ</div>
+                    <div class="more-info-button">ПОДРОБНЕЕ</div>
+                </div>
+            </div>
+        </div>
+        <div class="row aic printer-block">
+            <div class="col-md-6">
+                <div class="printer-name">DESIGNER <br> X-PRO</div>
+                <p>Designer X - высокопроизводительный 3D принтер профессионального класса, инструмент для реализации любых идей.</p>
+                <p>Новый принтер объединил в себе надежность легендарного PICASO 3D Designer, а также качество и стабильность новой платформы Х. Designer X всегда готов к работе. Высочайшее разрешение (слой от 0,01 мм) и производительность печати (до 100 см3/час) делают этот принтер уникальным.</p>
+                <div class="flex aic mt">
+                    <div class="main-button">ЗАКАЗАТЬ</div>
+                    <div class="more-info-button">ПОДРОБНЕЕ</div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="printer-image printer-image-3"></div>
+                <div class="printer-price price">269 000</div>
+            </div>
+        </div>
+    </div>
+</div>
