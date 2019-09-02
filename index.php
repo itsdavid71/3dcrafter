dc86369352afc07efe05d6540e83384c7108a5c1
David <itsdavid71@gmail.com>
2019-09-02 20:17:54 +0300

    Up

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php include 'template-shop/header.php' ?>
 
+<?php include 'template-shop/slider-main.php' ?>
 
+<?php include 'template-shop/examples-main.php' ?>
 
+<?php include 'template-shop/printers-main.php' ?>
 
+<?php include 'template-shop/clients-main.php' ?>
 
+<?php include 'template-shop/form-main.php' ?>
 
+<?php include 'template-shop/contact-main.php' ?>
 
-
-
+<?php include 'template-shop/footer.php' ?>
