3669184c1735954d9edeb3582e8e80b714a848ad
David <itsdavid71@gmail.com>
2019-09-02 21:19:15 +0300

    update

--- template-shop/slider-main.php
+++ template-shop/slider-main.php
@@ -1,7 +1,25 @@
+<link rel="stylesheet" href="styles-shop/david.css">
 <div class="slider-main">
     <div class="container">
-        <div class="slider-main-content">
-            <h1>3D принтеры PICASO 3D</h1>
+        <div class=" row slider-main-content">
+            <div class="col-md-6">
+                <h1>3D принтеры PICASO 3D</h1>
+                <p class="slider-main-text">DESIGNER XPRO</p>
+                <p>Больше не нужно выбирать между скоростью и качеством. Экономьте время и будьте уверены в результате.</p>
+                <div class="slider-main-logos">
+                    <div class="slider-main-logos-image x-b"></div>
+                    <div class="slider-main-logos-image xl-b"></div>
+                    <div class="slider-main-logos-image x-pro-b"></div>
+                    <div class="slider-main-logos-image xl-pro-b"></div>
+                </div>
+                <div class="slider-main-buy">
+                    <div class="slider-main-price price">299 000</div>
+                    <div class="slider-main-button">ЗАКАЗАТЬ</div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="slider-main-img"></div>
+            </div>
         </div>
     </div>
 </div>
