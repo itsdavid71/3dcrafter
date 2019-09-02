4839ae1d39e728ee87f7cc77a8f1488cc284ddfc
Julia Salimyanova <juliasalim@mail.ru>
2019-09-02 22:31:13 +0300

    Added block with examples

---
+++ template-shop/examples-main.php
@@ -0,0 +1,29 @@
+<div class="examples-main">
+    <div class="container">
+        <div class="examples-main-like"></div>
+        <h2>Хочешь печатать также?</h2>
+        <div class="row examples-main-photos">
+            <div class="col-md-6">
+                <div class="examples-main-photo first-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 100 микрон</p>
+                </div>
+                <div class="examples-main-photo second-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 100 микрон</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="examples-main-photo third-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 100 микрон</p>
+                </div>
+                <div class="examples-main-photo fourth-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 50 микрон</p>
+                </div>
+            </div>
+        </div>
+        <h2 class="centered">Научим печатать также!</h2>
+        <p class="centered">Каждому покупателю - бесплатное обучение.<br>Научим как пользоваться, как ухаживать и обслуживать</p>
+        <div class="examples-main-button-container">
+            <div class="examples-main-button">ЗАКАЗАТЬ ЗВОНОК</div>
+        </div>
+    </div>
+</div>
