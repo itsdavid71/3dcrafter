bd36be03401fb2e5b1090473a9271f210790ca0b
Julia Salimyanova <juliasalim@mail.ru>
2019-09-03 21:19:19 +0300

    Added clients-block and footer

---
+++ template-shop/clients-main.php
@@ -0,0 +1,19 @@
+<div class="clients-main">
+   <div class="container">
+      <h2>Наши клиенты</h2>
+      <div class="row clients-main-logos">
+         <div class="col-md-3">
+            <div class="clients-main-logo first-logo"></div>
+         </div>
+         <div class="col-md-3">
+            <div class="clients-main-logo second-logo"></div>
+         </div>
+         <div class="col-md-3">
+            <div class="clients-main-logo third-logo"></div>
+         </div>
+         <div class="col-md-3">
+            <div class="clients-main-logo fourth-logo"></div>
+         </div>
+      </div>
+   </div>
+</div>
--- template-shop/footer.php
+++ template-shop/footer.php
@@ -1,11 +1,47 @@
-
-
-
-
-
-
-
-
+<footer>
+    <div class="container">
+        <div class="row footer_blocks">
+            <div class="footer_block col-md-3">
+                <div class="footer_logo"></div>
+                <p>+7 (495) 00-44-187<br>Москва, ул. 2-я Ямская, д.6<br>sale@3dcrafter.ru</p>
+                <div class="footer_social">
+                    <div class="footer_social_logo footer_social_inst"></div>
+                    <div class="footer_social_logo footer_social_fb"></div>
+                    <div class="footer_social_logo footer_social_vk"></div>
+                    <div class="footer_social_logo footer_social_twitter"></div>
+                </div>
+            </div>
+            <div class="footer_block col-md-3">
+                <p class="footer_title">МАГАЗИН</p>
+                <a href="#" class="footer_link">Designer X</a>
+                <a href="#" class="footer_link">Designer XPRO</a>
+                <a href="#" class="footer_link">Designer XL</a>
+                <a href="#" class="footer_link">Designer XLPRO</a>
+                <a href="#" class="footer_link">Комплектующие</a>
+                <a href="#" class="footer_link">Пластик</a>
+            </div>
+            <div class="footer_block col-md-3">
+                <p class="footer_title">УСЛУГИ</p>
+                <a href="#" class="footer_link">3D-печать</a>
+                <a href="#" class="footer_link">3D-моделирование</a>
+                <a href="#" class="footer_link">Стоимость</a>
+                <a href="#" class="footer_link">Обучение</a>
+                <a href="#" class="footer_link">Ремонт</a>
+            </div>
+            <div class="footer_block col-md-3">
+                <p class="footer_title">КОМПАНИЯ</p>
+                <a href="#" class="footer_link">О нас</a>
+                <a href="#" class="footer_link">Технологии</a>
+                <a href="#" class="footer_link">Новости</a>
+                <p class="footer_title last_title">FAQ</p>
+                <a href="#" class="footer_link">Мин. заказ</a>
+                <a href="#" class="footer_link">Доставка</a>
+                <a href="#" class="footer_link">Скидки</a>
+            </div>
+        </div>
+        <p class="footer_copyright">© 2014-2019 «3Dcrafter.ru»</p>
+    </div>
+</footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 </body>
