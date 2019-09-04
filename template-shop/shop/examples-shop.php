b986441daa60c46e65cb87e20c0b14e1e0949be4
Julia Salimyanova <juliasalim@mail.ru>
2019-09-04 20:38:11 +0300

    Added blocks in shop-page

---
+++ template-shop/shop/examples-shop.php
@@ -0,0 +1,37 @@
+<div class="examples-main">
+    <div class="container">
+        <div class="examples-main-desc">
+            <div class="examples-main-icon"></div>
+            <h2>Хочешь печатать также?</h2>
+        </div>
+        <div class="row examples-main-photos">
+            <div class="col-md-6">
+                <div class="examples-main-photo examples-shop-first">
+                    <h4>Качество в деталях</h4>
+                    <p>Качество печати Designer X - наша гордость. Добиться высочайших результатов помогает точная механика и аппаратная платформа Х. Минимальный слой печати 10 микрон. Это в 10 раз тоньше человеческого волоса.</p>
+                    <p>Бесплатный тест: чтобы вы убедились в качестве печати 3D принтера, мы распечатаем на нём тестовое изделие до 100 см³ бесплатно.</p>
+                    <p>Закажите обратный звонок сейчас и опытный специалист ответит на ваш вопрос в течении 15 минут и организует на тестовую печать для вас.</p>
+                    <div onclick="order_button()" class="examples-shop-button">Заказать звонок</div>
+                </div>
+                <div class="examples-main-photo second-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 100 микрон</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="examples-main-photo third-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 100 микрон</p>
+                </div>
+                <div class="examples-main-photo fourth-photo">
+                    <p class="examples-main-photo-desc">Материал: PLA<br>Толщина слоя: 50 микрон</p>
+                </div>
+            </div>
+        </div>
+        <div class="examples-main-desc">
+            <h2 class="centered">Научим печатать также!</h2>
+            <p class="centered">Каждому покупателю - бесплатное обучение.<br>Научим как пользоваться, как ухаживать и обслуживать</p>
+            <div class="examples-main-button-container">
+                <div onclick="order_button()" class="examples-main-button">ЗАКАЗАТЬ ЗВОНОК</div>
+            </div>
+        </div>
+    </div>
+</div>
