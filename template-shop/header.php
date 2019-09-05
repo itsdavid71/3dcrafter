1c902809699f773f8a55894700b18de89721b99d
Julia Salimyanova <juliasalim@mail.ru>
2019-09-05 20:13:38 +0300

    Changed meta

--- template-shop/header.php
+++ template-shop/header.php
@@ -2,14 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="3Dcrafter.ru">
+    <meta property="og:url" content="https://3dcrafter.ru/store.php">
+    <meta property="og:title" content="3Dcrafter.ru — Магазин 3D-принтеров в Москве">
+    <meta property="og:image" content="https://3dcrafter.ru/docs/media/soc-3dcrafter.jpg">
+    <meta property="og:description" content="Продажа высокоточных 3D-принтеров в Москве от производителя Picaso 3D с бесплатным обучением и гарантией">
+    <title>3Dcrafter.ru — Магазин 3D-принтеров в Москве</title>
+    <meta name="keywords" content="купить 3D принтер в Москве для 3д печати цена">
+    <meta name="description" content="Продажа высокоточных 3D-принтеров в Москве от производителя Picaso 3D с бесплатным обучением и гарантией">
     <link rel="stylesheet" href="styles-shop/animate.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="styles-shop/styles.css">
     <link rel="stylesheet" href="styles-shop/styles_new.css">
-    <link rel="stylesheet" href="styles-shop/david.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
