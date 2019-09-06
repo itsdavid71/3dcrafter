40ffa9ddc25e78864e98974cffb36ddff7a87194
David <itsdavid71@gmail.com>
2019-09-06 22:40:52 +0300

    asd

--- scripts-shop/modals.js
+++ scripts-shop/modals.js
@@ -6,15 +6,9 @@
 
 $(document).ready(function() {
 
-    // $('#modal-podacha').click(function() {
-        // $('.modal').fadeOut(0);
-        // $('.modal-podacha').fadeIn(300);
-    // });
     $('.control-shop-button').click(function() {
         var modal_id = ($(this).attr('id'));
-        // $('.modal').fadeOut(0);
         var window = '.' + modal_id;
-        // console.log(window);
         $(window).fadeIn(300);
     });
     
